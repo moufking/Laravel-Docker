ec1651ef550dd641b93b98419dbbde31c109f92f
moufid <moufking337@gmail.com>
2020-12-27 20:19:35 +0100

    add other test who is false

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -18,4 +18,11 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function testOtherTest()
+    {
+        $response = $this->get('/dashboard');
+
+        $response->assertStatus(400);
+    }
 }
